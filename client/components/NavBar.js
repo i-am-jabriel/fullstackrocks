9f5f1be71d30e0f2a209a20517c7098b6005b7c7
bryanhclark <bryanhclark@gmail.com>
2018-01-12 09:49:02 -0500

    added navbar and productPreview component

--- client/components/NavBar.js
+++ client/components/NavBar.js
@@ -1,16 +1,88 @@
-import React from 'react'
+import React, { Component } from 'react';
+import AppBar from 'material-ui/AppBar';
+import IconButton from 'material-ui/IconButton';
+import IconMenu from 'material-ui/IconMenu';
+import MenuItem from 'material-ui/MenuItem';
+import FlatButton from 'material-ui/FlatButton';
+import Toggle from 'material-ui/Toggle';
+import MoreVertIcon from 'material-ui/svg-icons/navigation/more-vert';
+import NavigationClose from 'material-ui/svg-icons/navigation/close';
 
 /**
  * COMPONENT
  */
-const Home = (props) => {
+class Login extends Component {
+    static muiName = 'FlatButton';
 
-
-    return (
-        <div>
-            <h3>Welcome, navbar</h3>
-        </div>
-    )
+    render() {
+        return (
+            <FlatButton {...this.props} label="Login" />
+        );
+    }
 }
 
-export default Home
+const Logged = (props) => (
+    <IconMenu
+        {...props}
+        iconButtonElement={
+            <IconButton><MoreVertIcon /></IconButton>
+        }
+        targetOrigin={{ horizontal: 'right', vertical: 'top' }}
+        anchorOrigin={{ horizontal: 'right', vertical: 'top' }}
+    >
+        <MenuItem primaryText="Home" />
+        <MenuItem primaryText="Cart" />
+        <MenuItem primaryText="Sign out" />
+    </IconMenu>
+);
+
+Logged.muiName = 'IconMenu';
+
+/**
+ * This example is taking advantage of the composability of the `AppBar`
+ * to render different components depending on the application state.
+ */
+class NavBar extends Component {
+    state = {
+        logged: true,
+    };
+
+    handleChange = (event, logged) => {
+        this.setState({ logged: logged });
+    };
+
+    render() {
+        return (
+            <div>
+                <AppBar
+                    title="FSAROCKS"
+                    iconElementRight={this.state.logged ? <Logged /> : <Login />}
+                    showMenuIconButton={false}
+                />
+            </div>
+        );
+    }
+}
+
+
+
+
+
+
+// <nav>
+//                 {
+//                     isLoggedIn
+//                         ? <div>
+//                             {/* The navbar will show these links after you log in */}
+//                             <Link to="/home">Home</Link>
+//                             <a href="#" onClick={handleClick}>Logout</a>
+//                         </div>
+//                         : <div>
+//                             {/* The navbar will show these links before you log in */}
+//                             <Link to="/login">Login</Link>
+//                             <Link to="/signup">Sign Up</Link>
+//                         </div>
+//                 }
+//             </nav>
+
+export default NavBar
--- client/components/main.js
+++ client/components/main.js
@@ -1,8 +1,9 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import {connect} from 'react-redux'
-import {withRouter, Link} from 'react-router-dom'
-import {logout} from '../store'
+import { connect } from 'react-redux'
+import { withRouter, Link } from 'react-router-dom'
+import { logout } from '../store'
+import NavBar from './NavBar'
 
 /**
  * COMPONENT
@@ -11,31 +12,16 @@ import {logout} from '../store'
  *  rendered out by the component's `children`.
  */
 const Main = (props) => {
-  const {children, handleClick, isLoggedIn} = props
+  const { children, handleClick, isLoggedIn } = props
 
   return (
     <div>
-      <h1>BOILERMAKER</h1>
-      <nav>
-        {
-          isLoggedIn
-            ? <div>
-              {/* The navbar will show these links after you log in */}
-              <Link to="/home">Home</Link>
-              <a href="#" onClick={handleClick}>Logout</a>
-            </div>
-            : <div>
-              {/* The navbar will show these links before you log in */}
-              <Link to="/login">Login</Link>
-              <Link to="/signup">Sign Up</Link>
-            </div>
-        }
-      </nav>
-      <hr />
+      <NavBar />
       {children}
     </div>
   )
 }
+
 
 /**
  * CONTAINER
@@ -48,7 +34,7 @@ const mapState = (state) => {
 
 const mapDispatch = (dispatch) => {
   return {
-    handleClick () {
+    handleClick() {
       dispatch(logout())
     }
   }
