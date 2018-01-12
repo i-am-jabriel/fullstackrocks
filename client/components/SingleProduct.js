a7e882f8f66133a8bd89c1cbe1680fff6118cbac
Shannen <shannen.ye@gmail.com>
2018-01-12 09:48:39 -0500

    created singleProduct View Page

--- client/components/SingleProduct.js
+++ client/components/SingleProduct.js
@@ -1,16 +1,56 @@
-import React from 'react'
+import React, {Component} from 'react'
+import {connect} from 'react-redux'
+import {fetchSingleProduct} from '../store/singleProduct'
 
 /**
  * COMPONENT
  */
-const Home = (props) => {
+class SingleProduct extends Component {
+    componentDidMount() {
+        this.props.loadOneProduct(this.props.match.params.prodId)
+    }
+    render() {
+        const product = this.props.oneProduct
+        return (
+            <div>
+                <img src={product.imageUrl} />
+                <h3>{product.title}</h3>
+                <h5>{product.description}</h5>
 
-
-    return (
-        <div>
-            <h3>Welcome, singleProd</h3>
-        </div>
-    )
+                <h6>In Stock: {product.invQuantity}</h6>
+                <button>
+                <h5>Add To Cart</h5>
+                </button>
+                <div>
+                    <h3>Product Reviews</h3>
+                    <h5>User Review</h5>
+                    <h5>User's Rating for Product(stars)</h5>
+                    <h6>User's feedback for product</h6>
+                </div>
+                <div>
+                    <h3>Write a Review</h3>
+                </div>
+            </div>
+        )
+    }
 }
 
-export default Home
+
+const mapState = (state) => {
+    return {
+        oneProduct: state.singleProduct
+        // allUsers: state.allUsers ???
+    }
+}
+
+const mapDispatch = (dispatch) => {
+    return {
+        loadOneProduct: function(productId) {
+            dispatch(fetchSingleProduct(productId))
+        }
+    }
+}
+
+const SingleProductContainer = connect(mapState, mapDispatch)(SingleProduct)
+
+export default SingleProductContainer
