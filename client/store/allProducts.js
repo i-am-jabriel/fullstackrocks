b3a7a5cd0d85050f9fb0fb0a5b7086214407dc21
Vinit Parkar <vinit.parkar@gmail.com>
2018-01-12 11:55:39 -0500

    All Products UI initial build

--- client/store/allProducts.js
+++ client/store/allProducts.js
@@ -1,10 +1,30 @@
+import axios from 'axios';
 
+//Action Types
+const ALL_PRODUCTS = 'ALL_PRODUCTS';
 
+//Action Creators
+const allProducts = (products) => {
+    return {
+        type: ALL_PRODUCTS,
+        products
+    };
+};
 
-
+//Thunk Action Creator
+export const fetchProducts = () => {
+    return function(dispatch) {
+        axios.get('/api/products')
+        .then(response => {dispatch(allProducts(response.data)); })
+        .catch(console.error);
+    }
+};
 
 export default function (state = [], action) {
     switch (action.type) {
+
+        case ALL_PRODUCTS:
+          return action.products
 
         default:
             return state
