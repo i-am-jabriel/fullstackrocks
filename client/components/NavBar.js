872c5c3cde2e8797370c94ad3c1c20e82de8afa9
bryanhclark <bryanhclark@gmail.com>
2018-01-12 15:32:22 -0500

    added to homePage and productPreview components

--- client/components/NavBar.js
+++ client/components/NavBar.js
@@ -4,9 +4,11 @@ import IconButton from 'material-ui/IconButton';
 import IconMenu from 'material-ui/IconMenu';
 import MenuItem from 'material-ui/MenuItem';
 import FlatButton from 'material-ui/FlatButton';
+import RaisedButton from 'material-ui/RaisedButton';
 import Toggle from 'material-ui/Toggle';
 import MoreVertIcon from 'material-ui/svg-icons/navigation/more-vert';
 import NavigationClose from 'material-ui/svg-icons/navigation/close';
+
 
 /**
  * COMPONENT
