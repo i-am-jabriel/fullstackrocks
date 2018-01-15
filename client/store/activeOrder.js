cc1f15b44ad6efc42aee4bb9748c559c1e7d2a87
Shannen <shannen.ye@gmail.com>
2018-01-15 16:21:13 -0500

    created cartView for authenticated user

---
+++ client/store/activeOrder.js
@@ -0,0 +1,26 @@
+import axios from 'axios';
+
+const GET_ACTIVE_ORDER = 'GET_ACTIVE_ORDER'
+
+export const getActiveOrder = (activeOrder) => {
+    return {type: GET_ACTIVE_ORDER, activeOrder}
+}
+
+export const fetchActiveUserOrder = (userId) => {
+    return dispatch => {
+        axios.get(`/api/users/${userId}/orders/active`)
+        .then(res => res.data)
+        .then(activeOrder => dispatch(getActiveOrder(activeOrder)))
+    }
+}
+
+const initialState = []
+
+export default (state = initialState, action) => {
+    switch (action.type){
+        case GET_ACTIVE_ORDER:
+            return action.activeOrder
+        default:
+            return state
+    }
+}
