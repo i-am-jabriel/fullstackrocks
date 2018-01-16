f19c16b089d6fc3e9efdf2d02dddeaf197614dfe
Shannen <shannen.ye@gmail.com>
2018-01-16 12:57:07 -0500

    added remove item from user cart

--- client/store/activeOrder.js
+++ client/store/activeOrder.js
@@ -1,27 +1,58 @@
 import axios from 'axios';
 
 const GET_ACTIVE_ORDER = 'GET_ACTIVE_ORDER'
+const REMOVE_PRODUCT = 'REMOVE_PRODUCT'
 
 export const getActiveOrder = (activeOrder) => {
     return {type: GET_ACTIVE_ORDER, activeOrder}
 }
 
+export const removeProduct = (productId) => {
+    return {type: REMOVE_PRODUCT, productId}
+}
+
 export const fetchActiveUserOrder = (userId) => {
     return dispatch => {
         axios.get(`/api/users/${userId}/cart`)
-        .then(res => res.data)
-        .then(activeOrder => dispatch(getActiveOrder(activeOrder)))
+        .then(res => dispatch(getActiveOrder(res.data)))
     }
 }
 
-const initialState = []
+export const removeCurrentItem = (prodId, orderId, userId) => {
+    return (dispatch) => {
+        axios.delete(`/api/users/${userId}/cart`, {data: {prodId, orderId}})
+        .then(() => dispatch(removeProduct(prodId)))
+        .catch(console.error)
+    }
+}
+
+const initialState = {}
 
 export default (state = initialState, action) => {
     switch (action.type){
         case GET_ACTIVE_ORDER:
             return action.activeOrder
+        case REMOVE_PRODUCT:
+            const updatedProducts = remove(state, action.productId)
+            return Object.assign({}, state, {
+                products: updatedProducts
+            })
         default:
             return state
     }
 }
 
+// function remove (order, prodId) {
+//     const updatedList = order.products.filter(product => {
+//         if (product.id !== prodId) return product
+//     })
+//     order.products = updatedList
+//     return order
+// }
+
+function remove (order, prodId) {
+    return order.products.filter(product => {
+        // return product.id !== prodId
+        if (product.id !== prodId) return product
+    })
+}
