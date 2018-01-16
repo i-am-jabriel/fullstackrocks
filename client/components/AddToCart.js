e0cef3016e1e8df57d0ae11b73e07d2e0200e0c1
Shannen <shannen.ye@gmail.com>
2018-01-16 01:01:05 -0500

    created add to cart component

---
+++ client/components/AddToCart.js
@@ -0,0 +1,22 @@
+// import React, {Component} from 'react'
+
+// class AddToCart extends Component {
+//     componentDidMount() {
+
+//     }
+
+//     render() {
+//         return (
+
+//         )
+//     }
+// }
+
+// const mapState = (state) => {
+//     return {
+
+//     }
+// }
+
+// add to cart button if the user is logged in because it takes a user's id
+//
